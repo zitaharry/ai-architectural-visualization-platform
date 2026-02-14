f5119e339460d8521f7d1b42c6365323879188df
zitaharry <harrymuigua@gmail.com>
2026-02-14 20:48:30 +0300

    Implement authentication with Puter

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,28 @@
-import React from "react";
 import { Box } from "lucide-react";
 import Button from "./ui/Button";
+import { useOutletContext } from "react-router";
 
 const Navbar = () => {
-  const isSignedIn = false;
-  const username = "harry";
-  const handleAuthClick = () => {};
+  const { isSignedIn, userName, signIn, signOut } =
+    useOutletContext<AuthContext>();
+
+  const handleAuthClick = async () => {
+    if (isSignedIn) {
+      try {
+        await signOut();
+      } catch (e) {
+        console.error(`Puter sign out failed: ${e}`);
+      }
+
+      return;
+    }
+
+    try {
+      await signIn();
+    } catch (e) {
+      console.error(`Puter sign in failed: ${e}`);
+    }
+  };
 
   return (
     <header className="navbar">
@@ -29,7 +46,7 @@ const Navbar = () => {
           {isSignedIn ? (
             <>
               <span className="greeting">
-                {username ? `Hi, ${username}` : "Signed in"}
+                {userName ? `Hi, ${userName}` : "Signed in"}
               </span>
 
               <Button size="sm" onClick={handleAuthClick} className="btn">
@@ -41,6 +58,7 @@ const Navbar = () => {
               <Button onClick={handleAuthClick} size="sm" variant="ghost">
                 Log In
               </Button>
+
               <a href="#upload" className="cta">
                 Get Started
               </a>
@@ -51,4 +69,5 @@ const Navbar = () => {
     </header>
   );
 };
+
 export default Navbar;
